6b69c3175be305205cc7479e76bea197df111aa9
Aamer Rakla <arakla@AAMER-MAC.WV.CC.CMU.EDU>
2016-02-16 16:55:47 -0500

    Added validations and scopes to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ActiveRecord::Base
+  #Validations
+   validates_presence_of :title, :content
+    
+   #Scopes
+   scope :active,        -> { where(active: true) }
+   scope :alphabetical,  -> { order(:title) }
+
 end
